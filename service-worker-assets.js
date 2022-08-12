b848acbdd1ea81160e3b54f830aa356d68fd80d7
sam serious <sepp@seppside.com>
2022-08-12 09:35:33 +0200

    init

---
+++ service-worker-assets.js
@@ -0,0 +1,9 @@
+﻿self.assetsManifest = {
+  "assets": [
+    {
+      //"hash": "sha256-ZTaRRFkhhmOtVHwzWS2PhouDcTlZl2Hm4pvWapQCijE=",
+      "url": "index.html"
+    }
+  ],
+  "version": "0656"
+};
