43a5d023bbec98eb226caa438da354b3e19c7c31
sam serious <sepp@seppside.com>
2022-08-22 08:20:13 +0200

    dasdsa

--- service-worker-assets.js
+++
@@ -1,9 +0,0 @@
-﻿self.assetsManifest = {
-  "assets": [
-    {
-      //"hash": "sha256-ZTaRRFkhhmOtVHwzWS2PhouDcTlZl2Hm4pvWapQCijE=",
-      "url": "index.html"
-    }
-  ],
-  "version": "0663"
-};
